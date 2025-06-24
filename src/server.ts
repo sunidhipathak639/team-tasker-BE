24569f710e205b01c5341e38eec92aaaa20b293e
Sunidhi Pathak <118540996+sunidhipathak639@users.noreply.github.com>
2025-06-25 00:28:19 +0530

    chnages

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import authRoutes from "./routes/auth";
-
-const app = express();
-
-app.use(express.json());
-app.use("/api/auth", authRoutes);
-
-export default app;
